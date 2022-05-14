764b3464dfa20a470d16dc5e97a5f32615e0f5de
Neo <80315475+NeoYaBoi@users.noreply.github.com>
2022-05-14 16:15:10 +1000

    Leaderboard Commit
    This commit is not finished

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -14,7 +14,6 @@ module.exports = {
         await mongo().then(async (mongoose) => {
             try {
 
-                //add levels to leaderboard, in next update. (cant add now as users without levels on leaderboard will cause crashes.)
                 const user = message.member.user
                 const results = await userSchema.find().sort({ "money" : -1 })
 
@@ -22,41 +21,117 @@ module.exports = {
                 .setAuthor(`${message.member.displayName} | Leader Board`, user.displayAvatarURL({ format: 'jpg', dynamic: true }))
                 .addFields({
                     name: `🏅 1st`,
-                    value: `> 💵 **${results[0].money} BBC** | ${results[0]._id}`
+                    value: `> 💵 **${results[0].money} BBC** | ${results[0]._id}`,
+                    inline: true
+                }, {
+                    name: results[0].levelbadge,
+                    value: `**Level | ${results[0].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
                 }, {
                     name: `🥈 2nd`,
-                    value: `> 💵 **${results[1].money} BBC** | ${results[1]._id}`
+                    value: `> 💵 **${results[1].money} BBC** | ${results[1]._id}`,
+                    inline: true
                 }, {
+                    name: results[1].levelbadge,
+                    value: `**Level | ${results[1].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },  {
                     name: `🥉 3rd`,
-                    value: `> 💵 **${results[2].money} BBC** | ${results[2]._id}`
+                    value: `> 💵 **${results[2].money} BBC** | ${results[2]._id}`,
+                    inline: true
                 }, {
+                    name: results[2].levelbadge,
+                    value: `**Level | ${results[2].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },  {
                     name: `4th`,
-                    value: `> 💵 **${results[3].money} BBC** | ${results[3]._id}`
+                    value: `> 💵 **${results[3].money} BBC** | ${results[3]._id}`,
+                    inline: true
                 }, {
+                    name: results[3].levelbadge,
+                    value: `**Level | ${results[3].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },   {
                     name: `5th`,
-                    value: `> 💵 **${results[4].money} BBC** | ${results[4]._id}`
+                    value: `> 💵 **${results[4].money} BBC** | ${results[4]._id}`,
+                    inline: true
                 }, {
+                    name: results[4].levelbadge,
+                    value: `**Level | ${results[4].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },   {
                     name: `6th`,
-                    value: `> 💵 **${results[5].money} BBC** | ${results[5]._id}`
+                    value: `> 💵 **${results[5].money} BBC** | ${results[5]._id}`,
+                    inline: true
                 }, {
+                    name: results[5].levelbadge,
+                    value: `**Level | ${results[5].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },   {
                     name: `7th`,
-                    value: `> 💵 **${results[6].money} BBC** | ${results[6]._id}`
+                    value: `> 💵 **${results[6].money} BBC** | ${results[6]._id}`,
+                    inline: true
                 }, {
+                    name: results[6].levelbadge,
+                    value: `**Level | ${results[6].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },   {
                     name: `8th`,
-                    value: `> 💵 **${results[7].money} BBC** | ${results[7]._id}`
+                    value: `> 💵 **${results[7].money} BBC** | ${results[7]._id}`,
+                    inline: true
                 }, {
+                    name: results[7].levelbadge,
+                    value: `**Level | ${results[7].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
+                },   {
                     name: `9th`,
-                    value: `> 💵 **${results[8].money} BBC** | ${results[8]._id}`
+                    value: `> 💵 **${results[8].money} BBC** | ${results[8]._id}`,
+                    inline: true
+                }, {
+                    name: results[8].levelbadge,
+                    value: `**Level | ${results[8].level}**`,
+                    inline: true
+                }, {
+                    name: "\u200b",
+                    value: "\u200b"
                 }, {
                     name: `10th`,
-                    value: `> 💵 **${results[9].money} BBC** | ${results[9]._id}`
-                })
+                    value: `> 💵 **${results[9].money} BBC** | ${results[9]._id}`,
+                    inline: true
+                }, {
+                    name: results[9].levelbadge,
+                    value: `**Level | ${results[9].level}**`,
+                    inline: true
+                }, )
                 .setColor("BLACK")
                 message.channel.send(embedForLeaderBoard)
 
             } finally {
                 mongoose.connection.close()
-
             }
         })
     }
